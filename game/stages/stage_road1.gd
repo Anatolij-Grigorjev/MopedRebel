7ea96ae7e8e2c7572d0aa819571b5bd4fa906d2c
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2019-01-09 19:02:01 +0200

    add correct camera and rebel nodes

--- game/stages/stage_road1.gd
+++ game/stages/stage_road1.gd
@@ -1,42 +1,15 @@
 extends Node2D
 
-var rebel_on_foot_scene = preload("res://rebel/rebel_on_foot.tscn")
-var rebel_on_moped_scene = preload("res://rebel/rebel_on_moped.tscn")
-
-var to_spawn
-var spawn_pos
-
-var spawn_pairs = []
-var idx = 0
+var rebel_on_foot_node
+var rebel_on_moped_node
 
 func _ready():
-	spawn_pairs = [
-	{
-		"scene": rebel_on_foot_scene,
-		"pos": $spwn_foot
-	},
-	{
-		"scene": rebel_on_moped_scene,
-		"pos": $spwn_moped
-	}
-	]
-	idx = spawn_pairs.size() - 1
-	next_spawn()
+	rebel_on_foot_node = $rebel_on_foot
+	rebel_on_moped_node = $rebel_on_moped
+	init_rebel_on_foot()
 	
-	pass
 	
-func next_spawn():
-	idx = (idx + 1) % spawn_pairs.size()
-	var next = spawn_pairs[idx]
-	to_spawn = next.scene
-	spawn_pos = next.pos
-
-func _process(delta):
+func init_rebel_on_foot():
+	rebel_on_moped_node.visible = false
+	rebel_on_foot_node.get_node('camera').current = true
 	
-	if (Input.is_action_just_released("spawn_moped")):
-		var instance = to_spawn.instance()
-		instance.global_position = spawn_pos.global_position
-		self.add_child(instance)
-		next_spawn()
-	
-	pass
