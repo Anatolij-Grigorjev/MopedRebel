302ef35f123be3d46b13e91b898fac84d6b7919c
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2019-01-18 20:08:11 +0200

    start of adding another vehicle to scene

---
+++ game/transport/simple_transport.gd
@@ -0,0 +1,17 @@
+extends KinematicBody2D
+
+export(float) var maintains_speed = 100
+var start_position = Vector2(0, 0)
+
+func _ready():
+	reset_position()
+
+func _physics_process(delta):
+	global_position.x += delta * maintains_speed
+
+func reset_position():
+	global_position = start_position
+
+func _screen_exited():
+	reset_position()
+	
