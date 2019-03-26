12c01e52a67aa94da18a09b23efca9ddf9feaedc
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2019-03-26 19:16:38 +0200

    generalize free offscreen with timeout

---
+++ game/common/dissapear_offscreen.gd
@@ -0,0 +1,28 @@
+extends VisibilityNotifier2D
+
+export(float) var grace_period = 1.0
+
+func _ready():
+	$wait_period.stop()
+	$wait_period.wait_time = grace_period
+	$wait_period.connect('timeout', self, '_offscreen_grace_timeout')
+	
+	connect('screen_entered', self, '_screen_entered')
+	connect('screen_exited', self, '_screen_exited')
+	pass
+
+func _screen_exited():
+	$wait_period.stop()
+	$wait_period.start()
+
+func _screen_entered():
+	#reset timer when car onscreen
+	#not to dissapear it
+	if (not $wait_period.is_stopped()):
+		$wait_period.stop()
+	
+func _offscreen_grace_timeout():
+	var free_target = self
+	if (owner != null):
+		free_target = owner
+	free_target.queue_free()
