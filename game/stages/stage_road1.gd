db96d608a449f1739f56dee7586b24c2cc7d1875
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2019-03-30 09:41:04 +0200

    some initial fixes for moped on sidewalk flow

--- game/stages/stage_road1.gd
+++ game/stages/stage_road1.gd
@@ -9,7 +9,7 @@ func _ready():
 	rebel_on_moped_node = $rebel_on_moped
 	S.connect_signal_to(S.SIGNAL_REBEL_MOUNT_MOPED, self, "switch_foot_rebel_to_moped_on_road")
 	S.connect_signal_to(S.SIGNAL_REBEL_UNMOUNT_MOPED, self, "switch_moped_rebel_to_foot_on_sidewalk")
-	S.connect_signal_to(S.SIGNAL_REBEL_JUMP_CURB_ON_MOPED, self, "move_moped_rebel_to_sidewalk")
+	S.connect_signal_to(S.SIGNAL_REBEL_JUMP_CURB_ON_MOPED, self, "move_moped_rebel_over_curb")
 	
 	init_rebel_on_foot()
 
@@ -44,13 +44,25 @@ func _get_highest_road_position_below_sidewalk(on_sidewalk_position):
 	return on_road_position
 
 func switch_moped_rebel_to_foot_on_sidewalk():
-	var on_sidewalk_position = _get_lowest_sidewalk_position_above_road(rebel_on_moped_node.global_position)
+	var on_sidewalk_position = rebel_on_moped_node.global_position
+	if (not _is_rebel_on_sidewalk()):
+		on_sidewalk_position = _get_lowest_sidewalk_position_above_road(rebel_on_moped_node.global_position)
 	rebel_on_foot_node.global_position = on_sidewalk_position
 	init_rebel_on_foot()
+	
+func _is_rebel_on_sidewalk():
+	var rebel_position = G.node_active_rebel.global_position
+	var sidewalk_cell_idx = $sidewalk_tileset.get_cellv($sidewalk_tileset.world_to_map(rebel_position))
+	
+	return sidewalk_cell_idx >= 0
 
-func move_moped_rebel_to_sidewalk():
-	var on_sidewalk_position = _get_lowest_sidewalk_position_above_road(rebel_on_moped_node.global_position)
-	rebel_on_moped_node.global_position = on_sidewalk_position
+func move_moped_rebel_over_curb():
+	var otherside_position = (
+		_get_highest_road_position_below_sidewalk(rebel_on_moped_node.global_position) 
+		if _is_rebel_on_sidewalk() else 
+		_get_lowest_sidewalk_position_above_road(rebel_on_moped_node.global_position)
+	)
+	rebel_on_moped_node.global_position = otherside_position
 	
 func _get_lowest_sidewalk_position_above_road(on_road_position):
 	#find closes curb tile above and use a global position above it
