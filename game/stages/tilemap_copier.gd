2835779a4facb17506d6f51c29570d6e3d1b8baf
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2019-04-11 19:28:26 +0300

    add another tile to set, work to restore collision system

--- game/stages/tilemap_copier.gd
+++ game/stages/tilemap_copier.gd
@@ -1,12 +1,13 @@
 extends TileMap
 
-export (int) var COPIED_TILE_ID
+export (PoolIntArray) var COPIED_TILE_IDs = []
 
 func _enter_tree():
 	var base_map = get_parent()
 	set_up_tileset(base_map)
 
 func set_up_tileset(base_map):
-	var cells = base_map.get_used_cells_by_id(COPIED_TILE_ID)
-	for cell in cells:
-		set_cell(cell.x, cell.y, COPIED_TILE_ID)
+	for cell_type_id in COPIED_TILE_IDs:
+		var cells = base_map.get_used_cells_by_id(cell_type_id)
+		for cell in cells:
+			set_cell(cell.x, cell.y, cell_type_id)
