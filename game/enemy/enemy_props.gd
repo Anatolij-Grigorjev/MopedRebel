a22ad4d0f0ace327c9d2350591faa4d86d14ce48
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2019-06-18 22:01:53 +0300

    add more loaded props, more work towards new conflict model

--- game/enemy/enemy_props.gd
+++ game/enemy/enemy_props.gd
@@ -1,6 +1,7 @@
 extends Resource
 
 export (String) var type_id
+export (String) var type_name
 export (float) var fear_sc
 export (float) var escape_delay_sec
 export (PoolStringArray) var disses 
@@ -10,17 +11,19 @@ func _init(type_key):
 	F.assert_string_not_blank(type_key)
 	var type_props = L.loaded_enemies_props[type_key]
 	
-	F.assert_dict_props(type_props, ['fear_sc', 'escape_delay_sec', 'disses'])
+	F.assert_dict_props(type_props, ['type_name', 'fear_sc', 'escape_delay_sec', 'disses'])
 	F.assert_arr_not_empty(type_props['disses'])
 	
+	type_name = type_props['type_name']
 	fear_sc = type_props['fear_sc']
 	escape_delay_sec = type_props['escape_delay_sec']
 	disses = type_props['disses']
 	
 func as_string():
 	return """BASE PROPS:
-	Enemy type id: %s
+	Enemy type id: %s,
+	type name: %s,
 	base fear SC: %s,
-	base escape delay (sec): %s
+	base escape delay (sec): %s,
 	num disses: %s
-	""" % [type_id, fear_sc, escape_delay_sec, disses.size()]
+	""" % [type_id, type_name, fear_sc, escape_delay_sec, disses.size()]
--- game/enemy/type_props.gd
+++ game/enemy/type_props.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends GridContainer
 
 var Logger = preload("res://globals/logger.gd")
 var LoadedTypeProps = preload("res://enemy/enemy_props.gd")
@@ -8,6 +8,7 @@ export (String) var type_id
 export (Resource) var props
 export (float) var props_rand_prc = 0.1
 
+export (String) var type_name = '???'
 export (float) var fear_sc = 0
 export (float) var escape_delay_sec = 0
 export (PoolStringArray) var disses = []
@@ -18,12 +19,17 @@ func _ready():
 	props = LoadedTypeProps.new(type_id)
 	LOG = Logger.new("%s-props" % F.get_node_name_safe(self.owner))
 	LOG.info(props.as_string())
-
+	
+	type_name = props.type_name
 	fear_sc = _randomize_prop_val(props.fear_sc)
 	escape_delay_sec = _randomize_prop_val(props.escape_delay_sec)
 	disses = props.disses
 
 	LOG.info(as_string())
+	
+	$type_value.text = type_name
+	$fear_sc_value.text = fear_sc
+	$escape_delay_value.text = escape_delay_sec
 
 
 func _randomize_prop_val(base_val):
